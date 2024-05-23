497c939a65e5ebe7fd34b65d3ac73079a8f52121
Petra Maier <petra.maier@hbz-nrw.de>
2024-05-23 14:45:58 +0200

    Change approch creating ttl
    Add script for comparing different versions

--- create_faechersystematik_ttl.py
+++ create_faechersystematik_ttl.py
@@ -2,33 +2,17 @@ import pandas as pd
 import rdflib.term
 from rdflib import Graph, Literal, RDF, URIRef, Namespace
 
-url_stb = "https://github.com/dini-ag-kim/destatis-schluesseltabellen/raw/main/studierende/STB.csv?raw=true"
-url_studienfach = "https://github.com/dini-ag-kim/destatis-schluesseltabellen/blob/main/studierende/Studienfach.csv?raw=true"
-url_faechergruppe = "https://github.com/dini-ag-kim/destatis-schluesseltabellen/blob/main/studierende/Faechergruppe.csv?raw=true"
+url_1st_level = "https://github.com/dini-ag-kim/destatis-schluesseltabellen/blob/main/studierende/Faechergruppe.csv?raw=true"
+url_2nd_level = "https://github.com/dini-ag-kim/destatis-schluesseltabellen/raw/main/studierende/STB.csv?raw=true"
+url_3rd_level = "https://github.com/dini-ag-kim/destatis-schluesseltabellen/blob/main/studierende/Studienfach.csv?raw=true"
 
-df_stb = pd.read_csv(url_stb, encoding="ISO-8859-1", sep=';', quotechar='"', header=None, engine ='python', dtype=str)
-df_studienfach = pd.read_csv(url_studienfach, encoding="ISO-8859-1", sep=';', quotechar='"', header=None, engine ='python', dtype=str)
-df_faechergruppe = pd.read_csv(url_faechergruppe, encoding="ISO-8859-1", sep=';', quotechar='"', header=None, engine ='python', dtype=str)
+df_1st_level = pd.read_csv(url_1st_level, encoding="ISO-8859-1", sep=';', quotechar='"', header=None, engine ='python', dtype=str, usecols=[0, 2], names=["notation", "label"])
+df_2nd_level = pd.read_csv(url_2nd_level, encoding="ISO-8859-1", sep=';', quotechar='"', header=None, engine ='python', dtype=str, usecols=[0, 2, 3], names=["notation", "label", "broader"])
+df_3rd_level = pd.read_csv(url_3rd_level, encoding="ISO-8859-1", sep=';', quotechar='"', header=None, engine ='python', dtype=str, usecols=[0, 2, 3], names=["notation", "label", "broader"])
 
-def create_studifaecher_dict(bereichs_key):
-    dict_list = []
-    for i, r in df_studienfach.iterrows():
-        if r[3] == bereichs_key:
-            studifaecher_dict = {r[0]: r[2]}
-            dict_list.append(studifaecher_dict)
-    return dict_list
-
-faechergruppe_dict = dict(zip(df_faechergruppe[0], df_faechergruppe[2]))
-studibereich_dict = dict(zip(df_stb[0], df_stb[2]))
-destatis_dict = {}
-
-for index, row in df_stb.iterrows():
-    if row[3] in destatis_dict.keys():
-        studifaecher_dict = create_studifaecher_dict(row[0])
-        destatis_dict[row[3]].append({row[0]: studifaecher_dict})
-    else:
-        studibereich_dicts = [{row[0]:  create_studifaecher_dict(row[0])}]
-        destatis_dict.update({row[3]: studibereich_dicts})
+dict_1st_level = df_1st_level.to_dict("records")
+dict_2nd_level = df_2nd_level.to_dict("records")
+dict_3rd_level = df_3rd_level.to_dict("records")
 
 g = Graph()
 
@@ -49,34 +33,34 @@ g.add((URIRef('scheme'), dct['publisher'], rdflib.term.URIRef('https://oerworldm
 g.add((URIRef('scheme'), vann['preferredNamespaceUri'], rdflib.term.URIRef('https://w3id.org/kim/hochschulfaechersystematik/')))
 g.add((URIRef('scheme'), schema['isBasedOn'], rdflib.term.URIRef('http://bartoc.org/node/18919')))
 
-for top_level in destatis_dict.keys():
-    top_level = "n"+top_level.lstrip("0")
-    g.add((URIRef('scheme'), skos['hasTopConcept'], (URIRef(top_level))))
+
+for idx, i in enumerate(dict_1st_level):
+    top_level = dict_1st_level[idx]['notation'].lstrip("0")
+    g.add((URIRef('n%s' % top_level), RDF['type'], skos['Concept']))
+    g.add((URIRef('n%s' % top_level), skos['topConceptOf'], (URIRef('scheme'))))
+    g.add((URIRef('n%s' % top_level), skos['prefLabel'], Literal(dict_1st_level[idx]['label'], lang='de')))
+    g.add((URIRef('n%s' % top_level), skos['notation'], Literal(top_level)))
+    for idx_2, i_2 in enumerate(dict_2nd_level):
+        if dict_2nd_level[idx_2]['broader'].lstrip("0") == top_level:
+            level_2_notation = dict_2nd_level[idx_2]['notation']
+            g.add((URIRef('n%s' % level_2_notation), RDF['type'], skos['Concept']))
+            g.add((URIRef('n%s' % level_2_notation), skos['prefLabel'], Literal(dict_2nd_level[idx_2]['label'], lang='de')))
+            g.add((URIRef('n%s' % level_2_notation), skos['broader'], (URIRef('n%s' % dict_2nd_level[idx_2]['broader'].lstrip('0')))))
+            g.add((URIRef('n%s' % level_2_notation), skos['notation'], Literal(level_2_notation)))
+            g.add((URIRef('n%s' % level_2_notation), skos['inScheme'], (URIRef('scheme'))))
+            for idx_3, i_3 in enumerate(dict_3rd_level):
+                if dict_3rd_level[idx_3]['broader'] == level_2_notation:
+                    level_3_notation = dict_3rd_level[idx_3]['notation']
+                    g.add((URIRef('n%s' % level_3_notation), RDF['type'], skos['Concept']))
+                    g.add((URIRef('n%s' % level_3_notation), skos['prefLabel'],Literal(dict_3rd_level[idx_3]['label'], lang='de')))
+                    g.add((URIRef('n%s' % level_3_notation), skos['notation'], Literal(level_3_notation)))
+                    g.add((URIRef('n%s' % level_3_notation), skos['inScheme'], (URIRef('scheme'))))
+                    g.add((URIRef('n%s' % level_3_notation), skos['broader'], (URIRef('n%s' % dict_3rd_level[idx_3]['broader'].lstrip('0')))))
 
 g.add((URIRef('n0'), RDF['type'], skos['Concept']))
 g.add((URIRef('n0'), skos['prefLabel'], Literal('Fachübergreifend', lang='de')))
-g.add((URIRef('n0') , skos['notation'], Literal('0')))
+g.add((URIRef('n0'), skos['notation'], Literal('0')))
 g.add((URIRef('n0'), skos['topConceptOf'], (URIRef('scheme'))))
 
-# add each graph
-for key, value in destatis_dict.items():
-    g.add((URIRef('n%s' % key.lstrip('0')), RDF['type'], skos['Concept']))
-    g.add((URIRef('n%s' % key.lstrip('0')), skos['prefLabel'], Literal(faechergruppe_dict[key], lang='de')))
-    g.add((URIRef('n%s' % key.lstrip('0')), skos['notation'], Literal(key.lstrip('0'))))
-    g.add((URIRef('n%s' % key.lstrip('0')), skos['topConceptOf'], (URIRef('scheme'))))
-    for bereich in value:
-        for k,v in bereich.items():
-            g.add((URIRef('n%s' % k), RDF['type'], skos['Concept']))
-            g.add((URIRef('n%s' % k), skos['prefLabel'], Literal(studibereich_dict[k], lang='de')))
-            g.add((URIRef('n%s' % k), skos['notation'], Literal(k)))
-            g.add((URIRef('n%s' % k), skos['inScheme'], (URIRef('scheme'))))
-            g.add((URIRef('n%s' % k), skos['broader'], (URIRef('n%s' % key.lstrip('0')))))
-            for fach_items in v:
-                for fach_notation, fach_label in fach_items.items():
-                    g.add((URIRef('n%s' % fach_notation), RDF['type'], skos['Concept']))
-                    g.add((URIRef('n%s' % fach_notation), skos['prefLabel'], Literal(fach_label, lang='de')))
-                    g.add((URIRef('n%s' % fach_notation), skos['notation'], Literal(fach_notation)))
-                    g.add((URIRef('n%s' % fach_notation), skos['inScheme'], (URIRef('scheme'))))
-                    g.add((URIRef('n%s' % fach_notation), skos['broader'], (URIRef('n%s' % k))))
-
 g.serialize('faechersystematik.ttl', format='turtle')
+
